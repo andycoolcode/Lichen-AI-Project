f0e810f45957452377425fbe497639f0c1e421f6
Andy Y <andyhi4658@gmail.com>
2023-02-10 12:02:38 -0500

    Added feature for looping through folders

--- Lichen_color/Foliose_search.py
+++ Lichen_color/Foliose_search.py
@@ -1,58 +1,67 @@
-#open entire folder at a time rather than file?
+
 #maybe bash
 import numpy as np
 import cv2
-
-# img_path = input("What is the image path?")
-img_path = "TestImg2.png"
-cap =  cv2.imread(img_path)
-cap_HSV = cv2.cvtColor(cap, cv2.COLOR_BGR2HSV)
-cap_threshold = cv2.inRange(cap_HSV, (48, 5, 93), (89, 68,255))
-
-newimg = cap_threshold
-number_of_white_pix = np.sum(newimg != 0)
-number_of_total_pix = cap.shape[0] * cap.shape[1]
-print(number_of_total_pix)
-print("The number of pixels (shown in white) that fit this parameter is:",
-            number_of_white_pix)
-print(number_of_total_pix)
-desired_proportion = 100 * (number_of_white_pix / number_of_total_pix)
-print(desired_proportion)
+import os
 
 
+# get the path or directory
+folder_dir = input("What is the folder?")
+for images in os.listdir(folder_dir):
+    # img_path = input("What is the image path?")
+    img_path = folder_dir + '/' + images
+    cap =  cv2.imread(img_path)
+
+    #cropping by removing the outer 20% of the boundary (10% on each side)
+    #in order to get rid of any outside-of-tree areas
+    top_of_img = int(cap.shape[0] * 0.1)
+    bottom_of_img = int(cap.shape[0] * 0.9)
+    left_of_img = int(cap.shape[1] * 0.1)
+    right_of_img = int(cap.shape[1] * 0.9)
+    # Cropping an image
+    cap = cap[top_of_img:bottom_of_img, left_of_img:right_of_img]
 
 
-#Saving the image as a new file
-png_img_name = ''
+    cap_HSV = cv2.cvtColor(cap, cv2.COLOR_BGR2HSV)
+    cap_threshold = cv2.inRange(cap_HSV, (48, 5, 93), (89, 68,255))
 
-def rename_img(img_name, file_type):
-    index_num = img_name.index(file_type)
-    img_name = img_name[0:index_num]
-    img_name += '_Edited.png'
-    return img_name
+    newimg = cap_threshold
+    number_of_white_pix = np.sum(newimg != 0)
+    number_of_total_pix = cap.shape[0] * cap.shape[1]
+    # print(number_of_total_pix)
+    # print("The number of pixels (shown in white) that fit this parameter is:",
+    #             number_of_white_pix)
+    # print(number_of_total_pix)
+    desired_proportion = 100 * (number_of_white_pix / number_of_total_pix)
+    # print(desired_proportion)
 
-if '.jpg' in img_path:
-    png_img_name = rename_img(img_path, '.jpg')
-elif '.JPG' in img_path:
-    png_img_name = rename_img(img_path, '.JPG')
-elif '.jpeg' in img_path:
-    png_img_name = rename_img(img_path, '.jpeg')
-elif '.JPEG' in img_path:
-    png_img_name = rename_img(img_path, '.JPEG')
-elif '.png' in img_path:
-    png_img_name = rename_img(img_path, '.png')
-elif '.PNG' in img_path:
-    png_img_name = rename_img(img_path, '.png')
-else:
-    print('unsupported img type')
 
-#save selected color in file
-def write_to_file(imgName, pix_count, desired_pix_count):
-    f = open("saved_colors.txt", "a")
-    White_color = str(imgName) + ":" + str(pix_count) + \
-    "; proportion: " + str(desired_pix_count) + str("\r")
-    f.write(White_color)
-    f.close()
 
-write_to_file(png_img_name, number_of_white_pix, desired_proportion)
-cv2.imwrite(png_img_name, newimg)
+
+    # this will return a tuple of root and extension
+    split_tup = os.path.splitext(images)
+    
+    # extract the file name and extension
+    file_name = split_tup[0]
+    file_extension = split_tup[1]
+
+    png_img_name = file_name + '_Edited.PNG'
+
+    #new filedir
+    newfiledir = os.path.splitext(img_path)
+    new_file_name = newfiledir[0]
+    new_file_ext = newfiledir[1]
+    data_img_name = new_file_name + '_Edited.PNG'
+    #save selected color in file
+    def write_to_file(neighborhoodname, imgName, pix_count, proportional_pix_count):
+        pix_count = round(pix_count, 3)
+        proportional_pix_count = round(proportional_pix_count, 5)
+        f = open("Foliose_concentrations.csv", "a")
+        White_color = str(neighborhoodname) + "," + str(imgName) + "," \
+            + str(pix_count) + \
+            "," + str(proportional_pix_count) + str("\r")
+        f.write(White_color)
+        f.close()
+
+    write_to_file(folder_dir,png_img_name, number_of_white_pix, desired_proportion)
+    cv2.imwrite(data_img_name, newimg)
